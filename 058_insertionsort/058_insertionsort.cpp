ad49ed2131b0cb6685515953e8ea32b30ceaaa18
gheeeeeeee <gheoputra55@gmail.com>
2024-03-15 14:34:44 +0700

    prosedur insertionsort

--- 058_insertionsort/058_insertionsort.cpp
+++ 058_insertionsort/058_insertionsort.cpp
@@ -2,10 +2,33 @@
 //
 
 #include <iostream>
+using namespace std;
+
+void insertionsort() {
+
+	int i, n, temp, j;
+	int arr[12];
+
+	for (i = 1; i <= n - 1; i++){ //step 1
+		temp = arr [i]; //step 2
+	j = i - 1; //step 3
+	while (j >= 0 && arr[j] > temp) //step 4
+		{
+		arr[j + 1] = arr[j]; ///step 4a
+		j--; //step 4b
+		}
+
+	arr[j + 1] = temp; //step 5
+	}
+}	
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	system("pause");
+	return 0;
+
 }
 
 
