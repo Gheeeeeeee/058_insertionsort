ce18fb8e06d1ab2bdc1bc7ac20da1e113f781b2f
gheeeeeeee <gheoputra55@gmail.com>
2024-03-15 14:14:22 +0700

    Add project files.

---
+++ 058_insertionsort/058_insertionsort.cpp
@@ -0,0 +1,11 @@
+// 058_insertionsort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
